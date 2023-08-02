428c241ff787a35fc11bb2a45fd53d37bc3cec0f
MATMAF <mathieumafille1@gmail.com>
2023-08-02 11:22:51 +0200

    Adding ISP and country

--- WMIA.py
+++ WMIA.py
@@ -1,13 +1,39 @@
 from flask import Flask, render_template, request
 from flask_sslify import SSLify
+import requests
+import json
 
 app = Flask(__name__)
 sslify = SSLify(app)
 
+def get_location(ip):
+    url = "https://api.apilayer.com/ip_to_location/" + ip
+    payload = {}
+    headers= {
+    "apikey": "bidiugJMpp0jTIwCZU45Ll2g10jlCevB"
+    }
+
+    response = requests.request("GET", url, headers=headers, data = payload)
+
+    status_code = response.status_code
+    result = json.loads(response.text)
+
+    connection = result["connection"]
+
+    isp = connection["isp"]
+
+    country = result["country_name"]
+
+    return isp, country
+
+
 @app.route('/')
 def index():
     ip = request.remote_addr
-    return render_template('index.html', ip=ip)
+
+    location = get_location(ip)
+
+    return render_template('index.html', ip=ip, isp=location[0], country=location[1])
 
 @app.route('/ip')
 def return_ip():
