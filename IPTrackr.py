82cb8bd528a2d7da1c50dfa9d6d9fc6bc15f56ad
MATMAF <mathieumafille1@gmail.com>
2023-08-03 21:42:36 +0200

    Adding get_ip

--- IPTrackr.py
+++ IPTrackr.py
@@ -6,31 +6,27 @@ import json
 app = Flask(__name__)
 sslify = SSLify(app)
 
+def get_ip():
+    return request.remote_addr
+
 def get_location(ip):
     url = "http://ip-api.com/json/" + ip
-
     response = requests.request("GET", url)
-
     result = json.loads(response.text)
-
     isp = result["isp"]
-
     country = result["country"]
-
     return isp, country
 
 
 @app.route('/')
 def index():
-    ip = request.remote_addr
-
+    ip = get_ip()
     location = get_location(ip)
-
     return render_template('index.html', ip=ip, isp=location[0], country=location[1])
 
 @app.route('/ip')
 def return_ip():
-    ip = request.remote_addr
+    ip = get_ip()
     return ip
 
 if __name__ == '__main__':
